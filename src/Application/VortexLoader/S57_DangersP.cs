af3a37355edc0ae8faf0aa413c2dff885f276044
Michael Winther <miwin@gst.dk>
2024-12-06 13:25:04 +0100

    DangersP update

--- src/Application/VortexLoader/S57_DangersP.cs
+++ src/Application/VortexLoader/S57_DangersP.cs
@@ -3,6 +3,8 @@ using ArcGIS.Core.Geometry;
 using CommandLine;
 using S100Framework.DomainModel.S101;
 using S100Framework.DomainModel.S101.ComplexAttributes;
+using S100Framework.DomainModel.S101.FeatureTypes;
+using System;
 using static S100Framework.Applications.VortexLoader;
 using IO = System.IO;
 
@@ -13,17 +15,26 @@ namespace S100Framework.Applications
         private static void S57_DangersP(Geodatabase source, Geodatabase target, QueryFilter filter) {
             Console.WriteLine("DangersP");
 
-            using var s = source.OpenDataset<FeatureClass>("DangersP");
-            using var point = target.OpenDataset<FeatureClass>("point");
+            
+            var ps = "S-101";
+
+            var s = source.OpenDataset<FeatureClass>("DangersP");
+            using var featureClass = target.OpenDataset<FeatureClass>("point");
             using var informationtype = target.OpenDataset<Table>("informationtype");
 
-            using var bufferPoint = point.CreateRowBuffer();
-            using var insertPoint = point.CreateInsertCursor();
+            using var buffer = featureClass.CreateRowBuffer();
+            using var insert = featureClass.CreateInsertCursor();
 
             using var cursor = s.Search(filter, true);
             while (cursor.MoveNext()) {
                 var current = (Feature)cursor.Current;
                 var subtype = Convert.ToInt32(current["FCSubtype"]);
+                var inform = Convert.ToString(current["INFORM"]);
+                Decimal valsou = -32767;
+
+                if (DBNull.Value != current["VALSOU"] && current["VALSOU"] is not null) {
+                    valsou = Convert.ToDecimal(current["VALSOU"]);
+                }
 
                 switch (subtype) {
                     case 1: { // CTNARE
@@ -35,9 +46,55 @@ namespace S100Framework.Applications
                         break;
 
                     case 20: { // OBSTRN
+
+                            /*
+                             OBSTRN of geometric primitive area or line with attribute INFORM = Submerged weir will be
+                             converted to an instance of the S-101 Feature type Dam (see clause 4.8.5). Where this is the case,
+                             the attributes CATOBS, EXPSOU, NATQUA, NATSUR, PRODCT, QUASOU, SOUACC, TECSOU
+                             and VALSOU will not be converted. It is considered that these attributes are not relevant for Dam in
+                             S-101. 
+                            */
+
+                            waterLevelEffect waterLeveleffectCurrent = default;
+
+                            var obstruction = new S100Framework.DomainModel.S101.FeatureTypes.Obstruction {
+                                surroundingDepth = valsou,
+                                waterLevelEffect = waterLeveleffectCurrent
+                            };
+
+                            if (DBNull.Value != current["WATLEV"] && current["WATLEV"] is not null) {
+                                obstruction.waterLevelEffect = Convert.ToInt32(current["WATLEV"]) switch {
+                                    1 => waterLevelEffect.PartlySubmergedAtHighWater,  // partly submerged at high water
+                                    2 => waterLevelEffect.AlwaysDry,  // always dry
+                                    3 => waterLevelEffect.AlwaysUnderWaterSubmerged,  // always under water/submerged
+                                    4 => waterLevelEffect.CoversAndUncovers,  // covers and uncovers
+                                    5 => waterLevelEffect.Awash,  // awash
+                                    6 => waterLevelEffect.SubjectToInundationOrFlooding,  // subject to inundation or flooding
+                                    7 => waterLevelEffect.Floating,  // floating
+                                    -32767 => (waterLevelEffect)(-32767),
+                                    // TODO: QUESTION: how to handle -32767 on a required attribute without an S-101 equivalent "unknown". Illegal value assigned. MUST be fixed.
+
+                                    _ => throw new IndexOutOfRangeException(),
+                                };
+                            }
+
+
+                            if (DBNull.Value != current["PLTS_COMP_SCALE"] && current["PLTS_COMP_SCALE"] is not null) {
+                                obstruction.scaleMinimum = Convert.ToInt32(current["PLTS_COMP_SCALE"]);
+                            }
+
+
+                            AddFeatureName(obstruction.featureName, current);
+                            AddInformation(obstruction.information, current);
+
+                            buffer["ps"] = ps;
+                            buffer["code"] = "Obstruction";
+                            buffer["json"] = System.Text.Json.JsonSerializer.Serialize(obstruction);
+                            buffer["shape"] = current.GetShape();
+                            insert.Insert(buffer);
                         }
                         break;
-
+                        
                     case 35: { // UWTROC
                         }
                         break;
@@ -47,9 +104,49 @@ namespace S100Framework.Applications
                         break;
 
                     case 45: { // WRECKS
+                            waterLevelEffect waterLeveleffectCurrent = default;
+
+                            var wreck = new S100Framework.DomainModel.S101.FeatureTypes.Wreck {
+                                surroundingDepth = valsou,
+                                waterLevelEffect = waterLeveleffectCurrent
+
+                            };
+
+                            if (DBNull.Value != current["WATLEV"] && current["WATLEV"] is not null) {
+                                wreck.waterLevelEffect = Convert.ToInt32(current["WATLEV"]) switch {
+                                    1 => waterLevelEffect.PartlySubmergedAtHighWater,  // partly submerged at high water
+                                    2 => waterLevelEffect.AlwaysDry,  // always dry
+                                    3 => waterLevelEffect.AlwaysUnderWaterSubmerged,  // always under water/submerged
+                                    4 => waterLevelEffect.CoversAndUncovers,  // covers and uncovers
+                                    5 => waterLevelEffect.Awash,  // awash
+                                    6 => waterLevelEffect.SubjectToInundationOrFlooding,  // subject to inundation or flooding
+                                    7 => waterLevelEffect.Floating,  // floating
+                                    -32767 => (waterLevelEffect)(-32767),
+                                    // TODO: QUESTION: how to handle -32767 on a required attribute without an S-101 equivalent "unknown". Illegal value assigned. MUST be fixed.
+                                    _ => throw new IndexOutOfRangeException(),
+                                };
+                            }
+
+
+                            if (DBNull.Value != current["PLTS_COMP_SCALE"] && current["PLTS_COMP_SCALE"] is not null) {
+                                wreck.scaleMinimum = Convert.ToInt32(current["PLTS_COMP_SCALE"]);
+                            }
+
+                            AddFeatureName(wreck.featureName, current);
+                            AddInformation(wreck.information, current);
+
+                            buffer["ps"] = ps;
+                            buffer["code"] = "Obstruction";
+                            buffer["json"] = System.Text.Json.JsonSerializer.Serialize(wreck);
+                            buffer["shape"] = current.GetShape();
+                            insert.Insert(buffer);
                         }
+
                         break;
                 }
+
+
+
             }
         }
 
