a26eab25b2d5cfc01d2fe4e68b30102326d45d14
Michael Winther <miwin@gst.dk>
2025-01-09 07:37:07 +0100

    Test references

--- tests/Core.Tests/TestNisImporter/TestNisImporter.cs
+++ tests/Core.Tests/TestNisImporter/TestNisImporter.cs
@@ -1,19 +1,19 @@
-//using VortexLoader;
+using S100Framework.Applications;
 
-//namespace TestNisImporter
-//{
-//    public class TestNisImporter
-//    {
-//        [Fact]
-//        public void NoteLoaderTest() {
-//            var notesPath = @"G:\indigo\ENC\NotesAndPictures";
+namespace TestNisImporter
+{
+    public class TestNisImporter
+    {
+        [Fact]
+        public void NoteLoaderTest() {
+            var notesPath = @"G:\indigo\ENC\NotesAndPictures";
 
-//            foreach (var notePath in Directory.GetFiles(notesPath, "*.txt", SearchOption.AllDirectories)) {
-//                var note = new Note(notePath);
-//                //Assert.True(string.IsNullOrEmpty(note.Header));
-//                Assert.True(!string.IsNullOrEmpty(note.Content));
+            foreach (var notePath in Directory.GetFiles(notesPath, "*.txt", SearchOption.AllDirectories)) {
+                var note = new Note(notePath);
+                //Assert.True(string.IsNullOrEmpty(note.Header));
+                Assert.True(!string.IsNullOrEmpty(note.Content));
 
-//            }
-//        }
-//    }
-//}
+            }
+        }
+    }
+}
