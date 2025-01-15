f9573373102f1c2bde259269dc6eaa386852ed14
Michael Winther <miwin@gst.dk>
2025-01-15 11:01:00 +0100

    Added err string

--- src/Application/VortexLoader/Strings.cs
+++ src/Application/VortexLoader/Strings.cs
@@ -9,5 +9,6 @@ namespace S100Framework.Applications
     internal static class Strings
     {
         internal static readonly string UNKNOWN = "UNKNOWN";
+        internal static readonly string ERR_NULL_SHAPE = "NULL_SHAPE";
     }
 }
