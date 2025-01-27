f678b78f7925a79363082e3a75d2683037b164d1
Michael Winther <miwin@gst.dk>
2025-01-27 09:32:44 +0100

    Added coastlineL

---
+++ src/Application/VortexLoader/S57_CoastlineL.cs
@@ -0,0 +1,92 @@
+﻿using ArcGIS.Core.Data;
+using VortexLoader.S57.esri;
+using S100Framework.DomainModel.S101;
+using S100Framework.DomainModel.S101.FeatureTypes;
+
+namespace S100Framework.Applications
+{
+    internal static partial class ImporterNIS
+    {
+        private static void S57_CoastlineL(Geodatabase source, Geodatabase target, QueryFilter filter) {
+            var tableName = "CoastlineL";
+
+            var ps = "S-101";
+
+            var coastlinel = source.OpenDataset<FeatureClass>(tableName);
+
+            using var featureClass = target.OpenDataset<FeatureClass>("curve");
+            using var informationtype = target.OpenDataset<Table>("informationtype");
+
+            using var buffer = featureClass.CreateRowBuffer();
+            using var insert = featureClass.CreateInsertCursor();
+
+            using var cursor = coastlinel.Search(filter, true);
+            int recordCount = 0;
+            int convertedCount = 0;
+            while (cursor.MoveNext()) {
+                recordCount += 1;
+
+                var feature = (Feature)cursor.Current;
+
+                var current = new CoastlineL(feature);
+
+                var objectid = current.OBJECTID ?? default;
+                var globalid = current.GLOBALID;
+                var subtype = current.FCSUBTYPE ?? default;
+                var watlev = current.WATLEV ?? default;
+                var plts_comp_scale = current.PLTS_COMP_SCALE ?? default;
+                var longname = current.LNAM ?? Strings.UNKNOWN;
+                var status = current.STATUS ?? default;
+
+                switch (subtype) {
+                    case 1: { // COALNE_Coastline
+                            var instance = new Coastline();
+
+
+                            AddColour(instance.colour, feature);
+                            AddFeatureName(instance.featureName, feature);
+                            AddInformation(instance.information, feature);
+                            buffer["ps"] = ps;
+                            buffer["code"] = instance.GetType().Name;
+                            buffer["json"] = System.Text.Json.JsonSerializer.Serialize(instance);
+                            buffer["shape"] = current.SHAPE;
+                            insert.Insert(buffer);
+                            Logger.Current.DataObject(objectid, tableName, longname, System.Text.Json.JsonSerializer.Serialize(instance));
+                            convertedCount++;
+                        }
+                        break;
+                    case 
+                    5: { // SLCONS_ShorelineConstruction
+                            var instance = new ShorelineConstruction();
+                            if (plts_comp_scale != default) {
+                                instance.scaleMinimum = plts_comp_scale;
+                            }
+
+                            AddColour(instance.colour, feature);
+                            AddStatus(instance.status, feature);
+                            AddFeatureName(instance.featureName, feature);
+                            AddInformation(instance.information, feature);
+                            buffer["ps"] = ps;
+                            buffer["code"] = instance.GetType().Name;
+                            buffer["json"] = System.Text.Json.JsonSerializer.Serialize(instance);
+                            buffer["shape"] = current.SHAPE;
+                            insert.Insert(buffer);
+                            Logger.Current.DataObject(objectid, tableName, longname, System.Text.Json.JsonSerializer.Serialize(instance));
+                            convertedCount++;
+                        }
+                        break;
+                    default:
+                        // code block
+                        System.Diagnostics.Debugger.Break();
+                        break;
+                }
+
+
+
+            }
+            Logger.Current.DataTotalCount(tableName, recordCount, convertedCount);
+        }
+
+
+    }
+}
