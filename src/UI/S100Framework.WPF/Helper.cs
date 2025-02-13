5fdb60f7a93209ede699f91e4777b0bb5bfb20c9
Jens Søe Christiansen <jenssc@pocalipse.dk>
2025-02-13 22:40:30 +0100

    bugfix: Missing S-122 creation of ViewModel

--- src/UI/S100Framework.WPF/Helper.cs
+++ src/UI/S100Framework.WPF/Helper.cs
@@ -9,6 +9,7 @@ namespace S100Framework.WPF
 
             return ps.ToLowerInvariant() switch {
                 "s101" or "s-101" => S100Framework.WPF.ViewModel.S101.Preamble._creators.ContainsKey(type.Name) ? S100Framework.WPF.ViewModel.S101.Preamble._creators[type.Name]() : null,
+                "s122" or "s-122" => S100Framework.WPF.ViewModel.S122.Preamble._creators.ContainsKey(type.Name) ? S100Framework.WPF.ViewModel.S122.Preamble._creators[type.Name]() : null,
                 "s124" or "s-124" => S100Framework.WPF.ViewModel.S124.Preamble._creators.ContainsKey(type.Name) ? S100Framework.WPF.ViewModel.S124.Preamble._creators[type.Name]() : null,
                 "s128" or "s-128" => S100Framework.WPF.ViewModel.S128.Preamble._creators.ContainsKey(type.Name) ? S100Framework.WPF.ViewModel.S128.Preamble._creators[type.Name]() : null,
                 "s131" or "s-131" => S100Framework.WPF.ViewModel.S131.Preamble._creators.ContainsKey(type.Name) ? S100Framework.WPF.ViewModel.S131.Preamble._creators[type.Name]() : null,
@@ -23,6 +24,7 @@ namespace S100Framework.WPF
 
             return ps.ToLowerInvariant() switch {
                 "s101" or "s-101" => S100Framework.WPF.ViewModel.S101.Preamble._creators.ContainsKey(type) ? S100Framework.WPF.ViewModel.S101.Preamble._creators[type]() : null,
+                "s122" or "s-122" => S100Framework.WPF.ViewModel.S122.Preamble._creators.ContainsKey(type) ? S100Framework.WPF.ViewModel.S122.Preamble._creators[type]() : null,
                 "s124" or "s-124" => S100Framework.WPF.ViewModel.S124.Preamble._creators.ContainsKey(type) ? S100Framework.WPF.ViewModel.S124.Preamble._creators[type]() : null,
                 "s128" or "s-128" => S100Framework.WPF.ViewModel.S128.Preamble._creators.ContainsKey(type) ? S100Framework.WPF.ViewModel.S128.Preamble._creators[type]() : null,
                 "s131" or "s-131" => S100Framework.WPF.ViewModel.S131.Preamble._creators.ContainsKey(type) ? S100Framework.WPF.ViewModel.S131.Preamble._creators[type]() : null,
