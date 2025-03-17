d256aadc72052913aa379dca1b23210a50314d9b
Michael Winther <miwin@gst.dk>
2025-03-17 12:08:36 +0100

    Added DateHelper

---
+++ src/Application/VortexLoader/DateHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VortexLoader
+{
+    public static class DateHelper
+    {
+        public static DateOnly ConvertToDateOnly(string dateString) {
+            if (dateString.Length != 8 || !int.TryParse(dateString, out _)) {
+                throw new FormatException("Input string must be in 'yyyymmdd' format.");
+            }
+
+            int year = int.Parse(dateString.Substring(0, 4));
+            int month = int.Parse(dateString.Substring(4, 2));
+            int day = int.Parse(dateString.Substring(6, 2));
+
+            return new DateOnly(year, month, day);
+        }
+    }
+}
