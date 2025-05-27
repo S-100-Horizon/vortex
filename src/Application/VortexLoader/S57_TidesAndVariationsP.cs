53215b234e560adbb9b84e5e7c3c8988cb6de905
Michael Winther <miwin@gst.dk>
2025-05-27 15:13:12 +0200

    Added tidesandvariations

---
+++ src/Application/VortexLoader/S57_TidesAndVariationsP.cs
@@ -0,0 +1,125 @@
+﻿using ArcGIS.Core.Data;
+using S100Framework.Applications.S57.esri;
+using S100Framework.DomainModel.S101;
+using S100Framework.DomainModel.S101.FeatureTypes;
+using S100Framework.Applications.Singletons;
+
+namespace S100Framework.Applications
+{
+    internal static partial class ImporterNIS
+    {
+        private static void S57_TidesAndVariationsP(Geodatabase source, Geodatabase target, QueryFilter filter) {
+            var tableName = "TidesAndVariationsP";
+
+            using var tidesAndVariationsP = source.OpenDataset<FeatureClass>(source.GetName(tableName));
+            Subtypes.Instance.RegisterSubtypes(tidesAndVariationsP);
+
+            using var featureClass = target.OpenDataset<FeatureClass>(target.GetName("point"));
+
+
+            using var buffer = featureClass.CreateRowBuffer();
+            using var insert = featureClass.CreateInsertCursor();
+
+            using var cursor = tidesAndVariationsP.Search(filter, true);
+            int recordCount = 0;
+
+            while (cursor.MoveNext()) {
+                recordCount += 1;
+
+                var feature = (Feature)cursor.Current;
+
+                var current = new TracksAndRoutesA(feature);
+
+                var objectid = current.OBJECTID ?? default;
+                var globalid = current.GLOBALID;
+
+                if (ConversionAnalytics.Instance.IsConverted(globalid)) {
+                    continue;
+                }
+
+                var fcSubtype = current.FCSUBTYPE ?? default;
+                var plts_comp_scale = current.PLTS_COMP_SCALE ?? default;
+                var longname = current.LNAM ?? Strings.UNKNOWN;
+                var status = current.STATUS ?? default;
+
+                switch (fcSubtype) {
+                    case 5: { // LOCMAG_LocalMagneticAnomaly
+                            throw new NotImplementedException("No LOCMAG_LocalMagneticAnomaly in DK | GL NIS");
+                        }
+                        break;
+                    case 10: { // MAGVAR_MagneticVariation
+                            var instance = new MagneticVariation() {
+                            };
+                            if (current.PLTS_COMP_SCALE.HasValue && current.SHAPE != null) {
+                                string subtype = "";
+                                if (current.TableName != default && current.FCSUBTYPE.HasValue && !Subtypes.Instance.TryGetSubtype(current.TableName, current.FCSUBTYPE.Value, out subtype))
+                                    throw new NotSupportedException($"Unknown subtype for {current.TableName}, {current.FCSUBTYPE.Value}");
+                                instance.scaleMinimum = Scamin.Instance.GetMinimumScale(current.SHAPE, subtype, current.PLTS_COMP_SCALE.Value, isRelatedToStructure: false);
+                            }
+                            if (plts_comp_scale != default) {
+                                //instance.scaleMinimum = plts_comp_scale;
+                            }
+                            
+                            AddInformation(instance.information, feature);
+                            buffer["ps"] = ps101;
+                            buffer["code"] = instance.GetType().Name;
+                            buffer["json"] = System.Text.Json.JsonSerializer.Serialize(instance);
+                            SetShape(buffer, current.SHAPE);
+                            var featureN = featureClass.CreateRow(buffer);
+                            var name = Convert.ToString(featureN["name"]) ?? "Unknown name";
+                            if (FeatureRelations.Instance.HasRelated(current.GLOBALID)) {
+                                relatedEquipment?.CreateRelatedAreaEquipment(current, instance, name, target, source);
+                            }
+                            ConversionAnalytics.Instance.AddConverted(tableName, current.GLOBALID, name);
+                            Logger.Current.DataObject(objectid, tableName, longname, System.Text.Json.JsonSerializer.Serialize(instance));
+                        }
+                        break;
+                    case 15: { // T_HMON_TideHarmonicPrediction
+                            throw new NotImplementedException("No T_HMON_TideHarmonicPrediction in DK | GL NIS");
+                        }
+                        break;
+                    case 20: { // T_NHMN_TideNonHarmonicPrediction
+                            throw new NotImplementedException("No T_NHMN_TideNonHarmonicPrediction in DK | GL NIS");
+                        }
+                        break;
+                    case 25: { // T_TIMS_TideTimeSeries
+                            throw new NotImplementedException("No T_TIMS_TideTimeSeries in DK | GL NIS");
+                        }
+                        break;
+                    case 30: { // TIDEWY_Tideway
+                            throw new NotImplementedException("No TIDEWY_Tideway in DK | GL NIS");
+                        }
+                        break;
+                    case 35: { // TS_FEB_TidalStreamFloodEbb
+                            throw new NotImplementedException("No TS_FEB_TidalStreamFloodEbb in DK | GL NIS");
+                        }
+                        break;
+                    case 40: { // TS_PAD_TidalStreamPanelData
+                            throw new NotImplementedException("No TS_PAD_TidalStreamPanelData in DK | GL NIS");
+                        }
+                        break;
+                    case 45: { // TS_PNH_TidalStreamNonHarmonicPrediction
+                            throw new NotImplementedException("No TS_PNH_TidalStreamNonHarmonicPrediction in DK | GL NIS");
+                        }
+                        break;
+                    case 50: { // TS_PRH_TidalStreamHarmonicPrediction
+                            throw new NotImplementedException("No TS_PRH_TidalStreamHarmonicPrediction in DK | GL NIS");
+                        }
+                        break;
+                    case 55: { // TS_TIS_TidalStreamTimeSeries
+                            throw new NotImplementedException("No TS_TIS_TidalStreamTimeSeries in DK | GL NIS");
+                        }
+                        break;
+                    default:
+                        // code block
+                        System.Diagnostics.Debugger.Break();
+                        break;
+
+                }
+            }
+            Logger.Current.DataTotalCount(tableName, recordCount, ConversionAnalytics.Instance.GetConvertedCount(tableName));
+        }
+
+
+    }
+}
