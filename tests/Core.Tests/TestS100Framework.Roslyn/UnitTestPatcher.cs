5e41f2a8803a6dbe1351c2a3ae4d49a5da3f6ac1
Jens Søe Christiansen <jenssc@pocalipse.dk>
2025-06-07 13:49:17 +0200

    bugfix: ApplyEdits

--- tests/Core.Tests/TestS100Framework.Roslyn/UnitTestPatcher.cs
+++ tests/Core.Tests/TestS100Framework.Roslyn/UnitTestPatcher.cs
@@ -38,107 +38,115 @@ namespace TestS100Framework
             public void Patch_Associations() {
                 var path = Environment.GetEnvironmentVariable("s100ed7.gdb") ?? throw new System.ArgumentNullException();
 
-                using var geodatabase = new Geodatabase(new FileGeodatabaseConnectionPath(new Uri(IO.Path.GetFullPath(path))));
+                using var geodatabase = IO.Path.GetExtension(path) switch {
+                    ".sde" or ".SDE" => new Geodatabase(new DatabaseConnectionFile(new Uri(IO.Path.GetFullPath(path)))),
+                    ".gdb" or ".GDB" => new Geodatabase(new FileGeodatabaseConnectionPath(new Uri(IO.Path.GetFullPath(path)))),
+                    _ => throw new System.ArgumentNullException(),
+                };                                       
 
                 var informationAssociationbindings = new List<associationbinding>();
                 var featureAssociationbindings = new List<associationbinding>();
 
-                {
-                    using var associationbinding = geodatabase.OpenDataset<Table>("associationbinding");
+                var prefix = IO.Path.GetExtension(path).Equals(".sde") ? "s101." : "";
 
-                    using var cursor = associationbinding.Search(new QueryFilter {
-                        WhereClause = "upper(ps) = 'S-101'",
-                    }, true);
+                geodatabase.ApplyEdits(() => {
+                    {
+                        using var associationbinding = geodatabase.OpenDataset<Table>(prefix + "associationbinding");
 
-                    while (cursor.MoveNext()) {
-                        var row = cursor.Current;
+                        using var cursor = associationbinding.Search(new QueryFilter {
+                            WhereClause = "upper(ps) = 'S-101'",
+                        }, true);
 
-                        var type = Convert.ToString(row["type"])!;
+                        while (cursor.MoveNext()) {
+                            var row = cursor.Current;
 
-                        if (type.Equals("FeatureBinding", StringComparison.InvariantCultureIgnoreCase)) {
-                            featureAssociationbindings.Add(new UnitTestPatcher.associationbinding(
-                                    Convert.ToString(row["roleType"])!,
-                                    Convert.ToString(row["association"])!,
-                                    Convert.ToString(row["role"])!,
-                                    Convert.ToString(row["associationID"])!,
-                                    Convert.ToString(row["foreignID"])!,
-                                    Convert.ToString(row["primaryID"])!
-                                ));
+                            var type = Convert.ToString(row["type"])!;
+
+                            if (type.Equals("FeatureBinding", StringComparison.InvariantCultureIgnoreCase)) {
+                                featureAssociationbindings.Add(new UnitTestPatcher.associationbinding(
+                                        Convert.ToString(row["roleType"])!,
+                                        Convert.ToString(row["association"])!,
+                                        Convert.ToString(row["role"])!,
+                                        Convert.ToString(row["associationID"])!,
+                                        Convert.ToString(row["foreignID"])!,
+                                        Convert.ToString(row["primaryID"])!
+                                    ));
+                            }
+                            else if (type.Equals("InformationBinding", StringComparison.InvariantCultureIgnoreCase)) {
+                                informationAssociationbindings.Add(new UnitTestPatcher.associationbinding(
+                                        Convert.ToString(row["roleType"])!,
+                                        Convert.ToString(row["association"])!,
+                                        Convert.ToString(row["role"])!,
+                                        Convert.ToString(row["associationID"])!,
+                                        Convert.ToString(row["foreignID"])!,
+                                        Convert.ToString(row["primaryID"])!
+                                    ));
+                            }
                         }
-                        else if (type.Equals("InformationBinding", StringComparison.InvariantCultureIgnoreCase)) {
-                            informationAssociationbindings.Add(new UnitTestPatcher.associationbinding(
-                                    Convert.ToString(row["roleType"])!,
-                                    Convert.ToString(row["association"])!,
-                                    Convert.ToString(row["role"])!,
-                                    Convert.ToString(row["associationID"])!,
-                                    Convert.ToString(row["foreignID"])!,
-                                    Convert.ToString(row["primaryID"])!
-                                ));
+                    }
+
+                    string[] featureclasses = [prefix + "point", prefix + "pointset", prefix + "curve", prefix + "surface"];
+
+                    var groupFeatureAssociation = featureAssociationbindings.GroupBy(e => e.primaryID);
+
+                    foreach (var featureclass in featureclasses) {
+                        using var fc = geodatabase.OpenDataset<FeatureClass>(featureclass);
+
+                        using var cursor = fc.CreateUpdateCursor(null, true);
+                        while (cursor.MoveNext()) {
+                            var f = (Feature)cursor.Current;
+
+                            var name = Convert.ToString(f["name"])!;
+
+                            if (!groupFeatureAssociation.Any(e => e.Key.Equals(name, StringComparison.InvariantCultureIgnoreCase)))
+                                continue;
+
+                            var bindings = groupFeatureAssociation.Where(e => e.Key.Equals(name, StringComparison.InvariantCultureIgnoreCase)).SelectMany(e => e.Select(a => new featureBinding {
+                                association = a.association,
+                                associationId = a.associationID,
+                                featureId = a.foreignID,
+                                role = a.role,
+                                roleType = a.roleType,
+                            })).ToList();
+
+                            var json = System.Text.Json.JsonSerializer.Serialize(bindings);
+
+                            f["featurebindings"] = json;
+                            cursor.Update(f);
+                        }                        
+                    }
+
+                    var groupInformationAssociation = informationAssociationbindings.GroupBy(e => e.primaryID);
+
+                    string[] datasets = [.. featureclasses, prefix + "informationtype"];
+
+                    foreach (var dataset in datasets) {
+                        using var table = geodatabase.OpenDataset<Table>(dataset);
+
+                        using var cursor = table.CreateUpdateCursor(null, true);
+                        while (cursor.MoveNext()) {
+                            var row = (Row)cursor.Current;
+
+                            var name = Convert.ToString(row["name"])!;
+
+                            if (!groupInformationAssociation.Any(e => e.Key.Equals(name, StringComparison.InvariantCultureIgnoreCase)))
+                                continue;
+
+                            var bindings = groupInformationAssociation.Where(e => e.Key.Equals(name, StringComparison.InvariantCultureIgnoreCase)).SelectMany(e => e.Select(a => new informationBinding {
+                                association = a.association,
+                                associationId = a.associationID,
+                                informationId = a.foreignID,
+                                role = a.role,
+                                roleType = a.roleType,
+                            })).ToList();
+
+                            var json = System.Text.Json.JsonSerializer.Serialize(bindings);
+
+                            row["informationbindings"] = json;
+                            cursor.Update(row);
                         }
                     }
-                }
-
-                string[] featureclasses = ["point", "pointset", "curve", "surface"];
-
-                var groupFeatureAssociation = featureAssociationbindings.GroupBy(e => e.primaryID);
-
-                foreach (var featureclass in featureclasses) {
-                    using var fc = geodatabase.OpenDataset<FeatureClass>(featureclass);
-
-                    using var cursor = fc.CreateUpdateCursor(null, true);
-                    while (cursor.MoveNext()) {
-                        var f = (Feature)cursor.Current;
-
-                        var name = Convert.ToString(f["name"])!;
-
-                        if (!groupFeatureAssociation.Any(e => e.Key.Equals(name, StringComparison.InvariantCultureIgnoreCase)))
-                            continue;
-
-                        var bindings = groupFeatureAssociation.Where(e => e.Key.Equals(name, StringComparison.InvariantCultureIgnoreCase)).SelectMany(e => e.Select(a => new featureBinding {
-                            association = a.association,
-                            associationId = a.associationID,
-                            featureId = a.foreignID,
-                            role = a.role,
-                            roleType = a.roleType,
-                        })).ToList();
-
-                        var json = System.Text.Json.JsonSerializer.Serialize(bindings);
-
-                        f["featurebindings"] = json;
-                        f.Store();
-                    }
-                }
-
-                var groupInformationAssociation = informationAssociationbindings.GroupBy(e => e.primaryID);
-
-                string[] datasets = [.. featureclasses, "informationtype"];
-
-                foreach (var dataset in datasets) {
-                    using var table = geodatabase.OpenDataset<Table>(dataset);
-
-                    using var cursor = table.CreateUpdateCursor(null, true);
-                    while (cursor.MoveNext()) {
-                        var row = (Row)cursor.Current;
-
-                        var name = Convert.ToString(row["name"])!;
-
-                        if (!groupInformationAssociation.Any(e => e.Key.Equals(name, StringComparison.InvariantCultureIgnoreCase)))
-                            continue;
-
-                        var bindings = groupInformationAssociation.Where(e => e.Key.Equals(name, StringComparison.InvariantCultureIgnoreCase)).SelectMany(e => e.Select(a => new informationBinding {
-                            association = a.association,
-                            associationId = a.associationID,
-                            informationId = a.foreignID,
-                            role = a.role,
-                            roleType = a.roleType,
-                        })).ToList();
-
-                        var json = System.Text.Json.JsonSerializer.Serialize(bindings);
-
-                        row["informationbindings"] = json;
-                        row.Store();
-                    }
-                }
+                });
             }
         }
     }
