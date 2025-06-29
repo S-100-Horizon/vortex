29a2813e079dd71a03de05f656926d1ad87dd0b1
Jens Søe Christiansen <jenssc@pocalipse.dk>
2025-06-29 17:30:47 +0200

    starting LUA testing.

--- tests/Core.Tests/TestLua/UnitTest1.cs
+++ tests/Core.Tests/TestLua/UnitTest1.cs
@@ -1,9 +1,42 @@
+using MoonSharp.Interpreter;
+using S100Framework.DomainModel.S101.FeatureTypes;
+
 namespace TestLua
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1() {
+            string lua = @"
+                    function validate(instance)
+                        if instance == nil then
+                            return false
+                        end
+                        if instance.dataAssessment == nil then
+                            return false
+                        elseif instance.dataAssessment < 0 then
+                            return false
+                        end
+                        return true
+                    end     
+
+                    return validate(Instance)
+                ";
+
+            var qualityOfBathymetricData = new QualityOfBathymetricData {
+
+            };
+
+            UserData.RegisterType<QualityOfBathymetricData>();
+            var dynvalye = UserData.Create(qualityOfBathymetricData);
+
+
+            var script = new Script();
+            script.Globals["Instance"] = dynvalye;
+
+            var result = script.DoString(lua);
+
+            System.Diagnostics.Debugger.Break();
 
         }
     }
