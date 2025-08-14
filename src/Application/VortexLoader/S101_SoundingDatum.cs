6642042d71f7e495b5674cb8fbcde8b0667e4588
Michael Winther <miwin@gst.dk>
2025-08-14 13:14:12 +0200

    Refactored SoundingDatum clip M_QUAL with M_SDAT

--- src/Application/VortexLoader/S101_SoundingDatum.cs
+++ src/Application/VortexLoader/S101_SoundingDatum.cs
@@ -1,4 +1,5 @@
 ﻿using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
 using S100Framework.Applications.S57.esri;
 using S100Framework.Applications.Singletons;
 using S100Framework.DomainModel.S101.FeatureTypes;
@@ -9,7 +10,46 @@ namespace S100Framework.Applications
 {
     internal static partial class ImporterNIS
     {
-        private static void S101_SoundingDatum(Geodatabase source, Geodatabase target, QueryFilter filter) {
+
+internal static List<Geometry> EraseTouchingParts(
+    List<Geometry> inputPolygons,
+    List<Geometry> clipPolygons) {
+        var geometryResult = new List<Geometry>();
+
+        foreach (var inputPolygon in inputPolygons) {
+            Geometry modifiedPolygon = inputPolygon;
+
+            var intersectingClipPolygons = clipPolygons
+                .Where(c => GeometryEngine.Instance.Intersects(c, inputPolygon))
+                .ToList();
+
+            foreach (var clipPolygon in intersectingClipPolygons) {
+                if (clipPolygon.GeometryType != GeometryType.Polygon)
+                    continue;
+
+                var clipPoly = (Polygon)clipPolygon;
+
+                //var boundaryPolyline = new Polyline(clipPoly.Parts[0].Points);
+
+                var intersection = GeometryEngine.Instance.Intersection(modifiedPolygon, clipPoly);
+
+                if (intersection == null || intersection.IsEmpty)
+                    continue;
+
+                modifiedPolygon = GeometryEngine.Instance.Difference(modifiedPolygon, intersection);
+
+                if (modifiedPolygon == null || modifiedPolygon.IsEmpty)
+                    break; 
+            }
+
+            geometryResult.Add(modifiedPolygon);
+        }
+
+        return geometryResult;
+    }
+
+
+    private static void S101_SoundingDatum(Geodatabase source, Geodatabase target, QueryFilter filter) {
 
             var metadataATableName = "MetaDataA";
             var productCoverageTableName = "ProductCoverage";
@@ -29,31 +69,20 @@ namespace S100Framework.Applications
             };
 
             var productCoverageFilter = new QueryFilter() {
-                WhereClause = "CATCOV = 1" //$"{whereClause}"
+                WhereClause = $"({whereClause}) AND CATCOV = 1"
             };
-
-            var clipped = Geometries.GetDissolvedClipped(metadataA, M_Qual_WhereFilter, productCoverage, productCoverageFilter);
-
-            //// Cut out V_DAT
-            //metadataAWhereFilter = new QueryFilter() {
-            //    WhereClause = $"({whereClause}) AND (fcsubtype = 55)"
-            //};
-            //var removedSubtype55 = Geometries.GetDissolvedClipped(result, metadataA, metadataAWhereFilter);
-
-            // Cut out M_SDAT - // Will be added again
-            var M_SDAT_WhereFilterS = new QueryFilter() {
+            var M_SDAT_WhereFilter = new QueryFilter() {
                 WhereClause = $"({whereClause}) AND (fcsubtype = 45)"
             };
 
-            var m_sdatCount = metadataA.GetCount(M_SDAT_WhereFilterS);
+            // Take all M_QUAL and cut out M_SDAT
 
-            if (m_sdatCount > 0) {
-                clipped = Geometries.GetDissolvedClipped(clipped, metadataA, M_SDAT_WhereFilterS);
-            }
+            var all_M_QUAL = Geometries.AllGeometries(metadataA, M_Qual_WhereFilter);
+            var all_M_SDAT = Geometries.AllGeometries(metadataA, M_SDAT_WhereFilter);
 
-            // Store clipped
+            var polygons = EraseTouchingParts(all_M_QUAL, all_M_SDAT);
 
-            foreach (var item in clipped) {
+            foreach (var item in polygons) {
                 if (item.IsEmpty) {
                     continue;
                 }
@@ -75,45 +104,28 @@ namespace S100Framework.Applications
 
             }
 
-            // Add M_SDATs
-            if (m_sdatCount > 0) {
-                {
-                    using var cursor = metadataA.Search(M_SDAT_WhereFilterS, false);
-
-                    while (cursor.MoveNext()) {
-                        var feature = (Feature)cursor.Current;
-
-                        var current = new MetaDataA(feature);
-
-                        var instance = new SoundingDatum {
-                            verticalDatum = default,
-                        };
-
-                        // TODO: interoperabilityIdentifier
-
-                        instance.verticalDatum = ImporterNIS.GetVerticalDatum(current.VERDAT ?? 3);
-
-                        AddInformation(instance.information, feature);
-                        buffer["ps"] = ps101;
-                        buffer["code"] = instance.GetType().Name;
-                        buffer["json"] = System.Text.Json.JsonSerializer.Serialize(instance, jsonSerializerOptions);
-                        SetShape(buffer, current.SHAPE);
-                        ImporterNIS.SetUsageBand(buffer, current.PLTS_COMP_SCALE!.Value);
-
-                        var featureN = featureClass.CreateRow(buffer);
-                        var name = Convert.ToString(featureN["name"]) ?? "Unknown name";
-
-                        if (FeatureRelations.Instance.HasSlaves(current.GLOBALID)) {
-                            relatedEquipment!.CreateRelatedAreaEquipment(current, instance, featureN, default);
-                        }
-
-                        ConversionAnalytics.Instance.AddConverted(metadataATableName, current.GLOBALID, name);
-
-                        Logger.Current.DataObject(current.OBJECTID!.Value, metadataATableName, current.LNAM!, System.Text.Json.JsonSerializer.Serialize(instance));
-                    }
+            
+            foreach (var item in all_M_SDAT) {
+                if (item.IsEmpty) {
+                    continue;
                 }
-            }
 
+                var instance = new SoundingDatum {
+                    verticalDatum = default,
+                };
+
+                instance.verticalDatum = DomainModel.S101.verticalDatum.BalticSeaChartDatum2000;
+
+                buffer["ps"] = ps101;
+                buffer["code"] = instance.GetType().Name;
+                buffer["json"] = System.Text.Json.JsonSerializer.Serialize(instance, jsonSerializerOptions);
+                SetShape(buffer, item);
+                ImporterNIS.SetUsageBand(buffer, _compilationScale);
+
+                var featureN = featureClass.CreateRow(buffer);
+                var name = Convert.ToString(featureN["name"]) ?? "Unknown name";
+
+            }
         }
     }
 }
