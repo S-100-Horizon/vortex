82617748d7308258011699c88bf0974cdf897a79
Jens Søe Christiansen <jenssc@pocalipse.dk>
2025-08-19 17:12:51 +0200

    Finalizing first minimum RC NauticalProductsManager.

--- src/Core/S100Framework.NauticalProducts/Settings.cs
+++ src/Core/S100Framework.NauticalProducts/Settings.cs
@@ -5,12 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace S100Framework.Settings
+namespace S100Framework.DomainModel.S128.Horizon
 {
-    public class NauticalProducts
+    namespace Settings
     {
-        public Connection[] Connections { get; set; } = [];
+        public record Connection(string ProductSpecification, Uri ConnectionFile);
+        public class NauticalProducts
+        {
+            public Connection[] Connections { get; set; } = [];
+        }
     }
 
-    public record Connection(string ProductSpecification, Uri ConnectionFile);
+    public enum ExportTypes : int {
+        NewDataset = 1,
+        NewEdition = 2,
+        Update = 4,
+        Reissue = 8,
+        Cancellation = 16,
+    }
+
+    public record ElectronicProduct(string DatasetName, DateTime TimestampUTC, int Edition, int Update, ExportTypes ExportTypes);
 }
