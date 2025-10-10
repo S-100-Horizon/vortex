6501ecfc96d2dc793a73fc34fc0c63834794e550
Michael Winther <miwin@gst.dk>
2025-10-10 10:33:42 +0200

    build error

--- src/Application/VortexLoader/S57_DangersP.cs
+++ src/Application/VortexLoader/S57_DangersP.cs
@@ -145,7 +145,7 @@ namespace S100Framework.Applications
                             buffer["code"] = obstruction.GetType().Name;
                             buffer["edition"] = ImporterNIS.s101version;
                             buffer["json"] = System.Text.Json.JsonSerializer.Serialize(obstruction);
-                            buffer["informationbindings"] = System.Text.Json.JsonSerializer.Serialize(instance.GetInformationBindings(), jsonSerializerOptions);
+                            buffer["informationbindings"] = System.Text.Json.JsonSerializer.Serialize(obstruction.GetInformationBindings(), jsonSerializerOptions);
 
                             SetShape(buffer, current.SHAPE);
                             SetUsageBand(buffer, current.PLTS_COMP_SCALE!.Value);
