b2bbaefac556a42afce7ebbacaa38e27ae6748b5
Jens Søe Christiansen <jenssc@pocalipse.dk>
2025-10-21 12:55:26 +0200

    SupportFile

---
+++ src/Core/S100Framework.Catalogues/Settings.cs
@@ -0,0 +1,22 @@
+﻿using S100Framework.DomainModel.S100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace S100Horizon.Settings
+{
+    public class SupportFile
+    {
+        public string FileName { get; set; } = string.Empty;
+        public S100_SupportFileFormat? s100_SupportFileFormat { get; set; }
+
+        [JsonIgnore]
+        [XmlIgnore]
+        public string Code => nameof(SupportFile);
+
+    }
+}
