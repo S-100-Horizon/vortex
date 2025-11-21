47c09e8dffbc267d4b2caa3bc68f3e0d87395f6a
Jens Søe Christiansen <jenssc@pocalipse.dk>
2025-11-21 13:40:16 +0100

    scaffolding

--- tests/UI.Tests/PropertyGridApplication/MainWindow.xaml.cs
+++ tests/UI.Tests/PropertyGridApplication/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using S100Framework.Catalogues;
+using System.DirectoryServices.ActiveDirectory;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,5 +21,16 @@ namespace PropertyGridApplication
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            var feature = System.Text.Json.JsonSerializer.Deserialize<S100Framework.DomainModel.S101.FeatureTypes.LightSectored>(json)!;
+            var viewmodel = new S100Framework.WPF.ViewModel.S101.LightSectoredViewModel {
+
+            }.Load(feature);
+
+            //TODO: this.PropertyGrid.SelectedFeatureObject = viewmodel;
+        }
+
+        const string json = "{\"categoryOfLight\":[],\"exhibitionConditionOfLight\":null,\"featureName\":[],\"fixedDateRange\":null,\"height\":11,\"interoperabilityIdentifier\":null,\"marksNavigationalSystemOf\":null,\"multiplicityOfFeatures\":null,\"periodicDateRange\":[],\"sectorCharacteristics\":[{\"lightCharacteristic\":7,\"lightSector\":[{\"colour\":[1],\"directionalCharacter\":null,\"lightVisibility\":[],\"sectorLimit\":{\"sectorLimitOne\":{\"sectorBearing\":158,\"sectorLineLength\":null},\"sectorLimitTwo\":{\"sectorBearing\":171,\"sectorLineLength\":null}},\"valueOfNominalRange\":11,\"sectorInformation\":[],\"sectorArcExtension\":null}],\"signalGroup\":[\"(1)\"],\"signalPeriod\":4,\"signalSequence\":[]},{\"lightCharacteristic\":7,\"lightSector\":[{\"colour\":[3],\"directionalCharacter\":null,\"lightVisibility\":[],\"sectorLimit\":{\"sectorLimitOne\":{\"sectorBearing\":171,\"sectorLineLength\":null},\"sectorLimitTwo\":{\"sectorBearing\":290,\"sectorLineLength\":null}},\"valueOfNominalRange\":8.5,\"sectorInformation\":[],\"sectorArcExtension\":null}],\"signalGroup\":[\"(1)\"],\"signalPeriod\":4,\"signalSequence\":[]},{\"lightCharacteristic\":7,\"lightSector\":[{\"colour\":[4],\"directionalCharacter\":null,\"lightVisibility\":[],\"sectorLimit\":{\"sectorLimitOne\":{\"sectorBearing\":110,\"sectorLineLength\":null},\"sectorLimitTwo\":{\"sectorBearing\":158,\"sectorLineLength\":null}},\"valueOfNominalRange\":8.5,\"sectorInformation\":[],\"sectorArcExtension\":null}],\"signalGroup\":[\"(1)\"],\"signalPeriod\":4,\"signalSequence\":[]}],\"signalGeneration\":null,\"status\":[],\"verticalDatum\":null,\"scaleMinimum\":179999,\"information\":[]}";
     }
 }
