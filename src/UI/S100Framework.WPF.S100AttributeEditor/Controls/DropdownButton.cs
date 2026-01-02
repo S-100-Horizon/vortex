f9b827f79c133fca90252658e5d18a9363192259
Jens Søe Christiansen <jenssc@pocalipse.dk>
2026-01-02 17:17:38 +0100

    Adding PLUS button (ComboBox)

---
+++ src/UI/S100Framework.WPF.S100AttributeEditor/Controls/DropdownButton.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace S100Framework.WPF.Controls
+{
+    public class DropdownButton : ContentControl
+    {
+        static DropdownButton() {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(DropdownButton),
+                new FrameworkPropertyMetadata(typeof(DropdownButton)));
+        }
+
+        public object DropdownContent {
+            get => GetValue(DropdownContentProperty);
+            set => SetValue(DropdownContentProperty, value);
+        }
+
+        public static readonly DependencyProperty DropdownContentProperty =
+            DependencyProperty.Register(nameof(DropdownContent),
+                typeof(object), typeof(DropdownButton));
+
+        public bool IsOpen {
+            get => (bool)GetValue(IsOpenProperty);
+            set => SetValue(IsOpenProperty, value);
+        }
+
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register(nameof(IsOpen),
+                typeof(bool), typeof(DropdownButton));
+    }
+
+
+
+    //public class DropdownButton : Control
+    //{
+    //    static DropdownButton() {
+    //        DefaultStyleKeyProperty.OverrideMetadata(
+    //            typeof(DropdownButton),
+    //            new FrameworkPropertyMetadata(typeof(DropdownButton)));
+    //    }
+
+    //    public object DropdownContent {
+    //        get => GetValue(DropdownContentProperty);
+    //        set => SetValue(DropdownContentProperty, value);
+    //    }
+
+    //    public static readonly DependencyProperty DropdownContentProperty =
+    //        DependencyProperty.Register(nameof(DropdownContent),
+    //            typeof(object), typeof(DropdownButton));
+
+    //    public bool IsOpen {
+    //        get => (bool)GetValue(IsOpenProperty);
+    //        set => SetValue(IsOpenProperty, value);
+    //    }
+
+    //    public static readonly DependencyProperty IsOpenProperty =
+    //        DependencyProperty.Register(nameof(IsOpen),
+    //            typeof(bool), typeof(DropdownButton));
+    //}
+}
--- src/UI/S100Framework.WPF.S100AttributeEditor/ItemsControlTemplateSelector.cs.cs
+++ src/UI/S100Framework.WPF.S100AttributeEditor/ItemsControlTemplateSelector.cs.cs
@@ -12,23 +12,30 @@ namespace S100Framework.WPF
 {
     public class ItemsControlTemplateSelector : DataTemplateSelector
     {
-        //public DataTemplate? DefaultItemControlTemplate { get; set; } = default;
-        //public DataTemplate? ComplexItemControlTemplate { get; set; } = default;
+        public DataTemplate? DefaultItemControlTemplate { get; set; } = default;
+        public DataTemplate? ComplexItemControlTemplate { get; set; } = default;
 
         //public DataTemplate? FeatureTypeControlTemplate { get; set; } = default;
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container) {
-
-            if (container is FrameworkElement fe) {
-                if (item is ComplexAttribute complexAttribute) {
-                    return (DataTemplate)fe.TryFindResource("ComplexItemControlTemplate");
-                }
-                if (item is FeatureType featureType) {
-                    throw new NotImplementedException();
-                }
-
-                return (DataTemplate)fe.TryFindResource("DefaultItemControlTemplate");
+            if (item is ComplexAttribute complexAttribute) {
+                return ComplexItemControlTemplate!;
             }
+            if (item is FeatureType featureType) {
+                throw new NotImplementedException();
+            }
+            return DefaultItemControlTemplate!;
+
+            //if (container is FrameworkElement fe) {
+            //    if (item is ComplexAttribute complexAttribute) {
+            //        return (DataTemplate)fe.TryFindResource("ComplexItemControlTemplate");
+            //    }
+            //    if (item is FeatureType featureType) {
+            //        throw new NotImplementedException();
+            //    }
+
+            //    return (DataTemplate)fe.TryFindResource("DefaultItemControlTemplate");
+            //}
 
             throw new NotImplementedException();
         }
