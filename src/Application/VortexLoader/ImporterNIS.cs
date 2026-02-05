0acfe699fcc2c4f5ece42b55666b478e1a101445
Jens Søe Christiansen <jenssc@pocalipse.dk>
2026-02-05 19:24:19 +0100

    Added Logging!

--- src/Application/VortexLoader/ImporterNIS.cs
+++ src/Application/VortexLoader/ImporterNIS.cs
@@ -850,6 +850,9 @@ namespace S100Framework.Applications
         //}
 
         internal static colourPattern? GetColourPattern(string colorPattern) {
+            if (colorPattern.Contains(",")) {
+                Logger.Current.Error("Multiple colorPatterns not supported in S-101!");
+            }
             var colourPat = colorPattern switch {
                 "1" => 1,   //colourPattern.HorizontalStripes,
                 "2" => 2,   //colourPattern.VerticalStripes,
